5cf0ee76166373c86a9529a69955cb85c0342c58
Siddarth2904 <bsiddarth729@gmail.com>
2024-02-13 11:56:48 +0530

    middleware and error handler

--- server/index.js
+++ server/index.js
@@ -19,3 +19,13 @@ app.listen(9000, () => {
 })
 app.use('/server/user', userRouter);
 app.use('/server/auth', authRouter);
+
+app.use((err,req,res,next)=>{
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong";
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack
+})})
