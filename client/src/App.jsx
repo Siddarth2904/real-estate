e0f8e8366258ac0ce1d67f67068e8599d50bfaf7
Siddarth2904 <bsiddarth729@gmail.com>
2024-02-18 18:48:48 +0530

    contact functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
 import UpdateListing from './pages/UpdateListing'
+import Listing from './pages/Listing'
 
 export default function App() {
   return (
@@ -21,6 +22,7 @@ export default function App() {
         <Route path="/signin" element={<Signin />} />
         <Route path="/signout" element={<Signout />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/listing/:listingId" element={<Listing />}></Route>
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
